e5d2509585f43ecfad85837c7f90e1fb8f54b272
lhuddlesto <lhuddlesto@gannett.com>
2019-01-24 15:49:30 -0500

    added print quote method

---
+++ app.py
@@ -0,0 +1,16 @@
+import sys
+import random
+
+def print_quote():
+    quotes = [ "It's lit!",
+    "Straight up!",
+    "Pop it, pop it.",
+    "For this life I cannot change."
+    ]
+    index = random.randint(0, len(quotes) - 1)
+    selected_quote = 
+    
+
+    print(quotes[index])
+
+print_quote()
